98c83288f0fbf53537c8d8c7da7e086cafeec179
Jean-Baptiste Lespiau <jblespiau@google.com>
2022-04-07 00:12:08 +0200

    Install Python.boost as a system package.

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ cmake .
 make
 cd ..
 
+# Install python boost as a system wide module.
+#
 # We select the latest version on March 2022, which was from December 2021.
 # See https://www.boost.org/users/history/version_1_78_0.html
-
-wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2
-tar --bzip2 -xf boost_1_78_0.tar.bz2
+#
+# wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2
+# tar --bzip2 -xf boost_1_78_0.tar.bz2
